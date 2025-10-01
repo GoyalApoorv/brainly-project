b1a6822bbe29fb454fcf1643d7f1e624c4232837
Apoorv Goyal <apoorvgoyal1212@gmail.com>
2025-10-01 23:35:33 +0530

    added root path

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,12 @@
 import { Dashboard } from "./pages/dashboard";
 import { Signin } from "./pages/Signin";
 import { Signup } from "./pages/Signup";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 function App() {
     return <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Navigate to="/signup" />} />
         <Route path="Signup" element={<Signup />} />
         <Route path="Signin" element={<Signin />} />
         <Route path="Dashboard" element={<Dashboard />} />
